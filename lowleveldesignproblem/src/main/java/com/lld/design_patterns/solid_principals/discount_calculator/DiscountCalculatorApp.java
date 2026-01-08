3ea2a17224a16ee2f593d72c1e3df7f9e46f952b
Kaartikgupta <kaartik.1rn17cs041@gmail.com>
2026-01-09 00:40:35 +0530

    solid principal demostration via code

--- lowleveldesignproblem/src/main/java/com/lld/design_patterns/solid_principals/discount_calculator/DiscountCalculatorApp.java
+++ lowleveldesignproblem/src/main/java/com/lld/design_patterns/solid_principals/discount_calculator/DiscountCalculatorApp.java
@@ -1,12 +1,13 @@
 package main.java.com.lld.design_patterns.solid_principals.discount_calculator;
 
 
-// this code demonstrate SRp, OCP, LSP
+// this code demonstrate SRp, OCP, LSP, ISP and DIP principles
 // Single Responsibility Principle: Each class has a single responsibility.
 // Open/Closed Principle: The PriceCalculator class is open for extension (new discount types)
 // but closed for modification (existing code doesn't need to change).
 // Liskov Substitution Principle: Any subclass of DiscountType can be used in place of DiscountType without affecting the correctness of the program.
-
+// Interface Segregation Principle: Clients (PriceCalculator) depend only on the methods they use (calculateDiscount).
+// Dependency Inversion Principle: PriceCalculator depends on the abstraction (DiscountType) rather than concrete implementations(seasonaldiscount).
 public class DiscountCalculatorApp {
     public static void main(String[] args) {
         DiscountType seasonalDiscount = new SeasonalDiscount();
